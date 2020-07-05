b41c09cd8d627d38adc7bf7c5eee8cf281537e94
kgoins <kgoins@github.com>
2020-07-05 16:42:41 -0400

    added internal package for test utils, firefox tests now pass

---
+++ internal/testutils/testutils.go
@@ -0,0 +1,13 @@
+package testutils
+
+import "path/filepath"
+
+// GetTestDataFilePath returns the full path of a file in the testdata/ dir
+func GetTestDataFilePath(testFile string) (string, error) {
+	testdataPath, err := filepath.Abs("../../testdata")
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(testdataPath, testFile), nil
+}
