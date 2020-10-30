09278c0f0e966915b1979d8d7fddb662ed35e885
Simon Lehn <48837958+srlehn@users.noreply.github.com>
2020-10-30 22:02:44 +0100

    add allbrowsers dir

---
+++ allbrowsers/import.go
@@ -0,0 +1,19 @@
+package allbrowsers
+
+import (
+	_ "github.com/zellyn/kooky/browsh"
+	_ "github.com/zellyn/kooky/chrome"
+	_ "github.com/zellyn/kooky/dillo"
+	_ "github.com/zellyn/kooky/edge"
+	_ "github.com/zellyn/kooky/elinks"
+	_ "github.com/zellyn/kooky/epiphany"
+	_ "github.com/zellyn/kooky/firefox"
+	_ "github.com/zellyn/kooky/ie"
+	_ "github.com/zellyn/kooky/konqueror"
+	_ "github.com/zellyn/kooky/lynx"
+	_ "github.com/zellyn/kooky/netscape"
+	_ "github.com/zellyn/kooky/opera"
+	_ "github.com/zellyn/kooky/safari"
+	_ "github.com/zellyn/kooky/uzbl"
+	_ "github.com/zellyn/kooky/w3m"
+)
