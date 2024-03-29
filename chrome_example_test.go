6072a9adb478364b468999fa610d7db2394e49fa
ReveEver <reveever@live.com>
2024-03-29 20:14:04 +0800

    add macos edge support

--- chrome_example_test.go
+++ chrome_example_test.go
@@ -5,19 +5,39 @@ import (
 	"os"
 
 	"github.com/browserutils/kooky/browser/chrome"
+	"github.com/browserutils/kooky/browser/edge"
 )
 
 // on macOS:
-var cookieStorePath = "/Google/Chrome/Default/Cookies"
+var cookieStorePathChrome = "/Google/Chrome/Default/Cookies"
+var cookieStorePathMsEdge = "/Microsoft Edge/Default/Cookies"
 
 func Example_chromeSimpleMacOS() {
 	// construct file path for the sqlite database containing the cookies
 	dir, _ := os.UserConfigDir() // on macOS: "/<USER>/Library/Application Support/"
-	cookieStoreFile := dir + cookieStorePath
+	cookieStoreFile := dir + cookieStorePathChrome
 
 	// read the cookies from the file
 	// decryption is handled automatically
 	cookies, err := chrome.ReadCookies(cookieStoreFile)
+	if err != nil {
+		// TODO: handle the error
+		return
+	}
+
+	for _, cookie := range cookies {
+		fmt.Println(cookie)
+	}
+}
+
+func Example_msEdgeSimpleMacOS() {
+	// construct file path for the sqlite database containing the cookies
+	dir, _ := os.UserConfigDir() // on macOS: "/<USER>/Library/Application Support/"
+	cookieStoreFile := dir + cookieStorePathMsEdge
+
+	// read the cookies from the file
+	// decryption is handled automatically
+	cookies, err := edge.ReadCookies(cookieStoreFile)
 	if err != nil {
 		// TODO: handle the error
 		return
