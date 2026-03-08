browserutils/kooky

commit bf04c09ae6f3392a006be249523fd2125edbf9aa

fix panic, correct Valid filter category, add OnlyCookieStores method, add first cookie match "benchmark"

find.go
@@ -2,9 +2,9 @@ package kooky
 
 import (
 	"context"
+	"fmt"
 	"iter"
 	"net/http"
-	"runtime"
 	"sync"
 )
 
@@ -65,6 +65,23 @@ func FindAllCookieStores(ctx context.Context) []CookieStore {
 
 type CookieStoreSeq iter.Seq2[CookieStore, error]
 
+// sequence of non-nil cookie stores and nil errors
+func (s CookieStoreSeq) OnlyCookieStores() CookieStoreSeq {
+	return func(yield func(CookieStore, error) bool) {
+		if s == nil {
+			return
+		}
+		for cookieStore, err := range s {
+			if err != nil || cookieStore == nil {
+				continue
+			}
+			if !yield(cookieStore, nil) {
+				return
+			}
+		}
+	}
+}
+
 func (s CookieStoreSeq) AllCookieStores(ctx context.Context) []CookieStore {
 	var ret []CookieStore
 	if s == nil {
@@ -77,123 +94,146 @@ Outer:
 			break Outer
 		default:
 		}
+		if cookieStore == nil {
+			continue
+		}
 		ret = append(ret, cookieStore)
 	}
 	return ret
 }
 
 func (s CookieStoreSeq) TraverseCookies(ctx context.Context, filters ...Filter) CookieSeq {
-	return func(yield func(*Cookie, error) bool) {
-		if s == nil {
+	if s == nil {
+		return func(yield func(*Cookie, error) bool) {}
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	type ce struct {
+		c *Cookie
+		e error
+	}
+	startChan := make(chan struct{}, 1)
+	cookieChan := make(chan ce, 1)
+
+	go func() {
+		select {
+		case <-ctx.Done():
 			return
+		case <-startChan: // wait for iteration start
 		}
-		ctx, cancel := context.WithCancel(ctx)
-		type ce struct {
-			c *Cookie
-			e error
-		}
-		cookieChan := make(chan ce)
-
-		var wgTot sync.WaitGroup
-		defer wgTot.Wait()
-		wgTot.Add(1)
-		go func() {
-			defer wgTot.Done()
-
-			var wgTrav sync.WaitGroup
-			defer func() {
-				wgTrav.Wait()
-				cancel()
-				close(cookieChan)
-			}()
-			for cookieStore, _ := range s {
+		var wg sync.WaitGroup
+		defer func() {
+			wg.Wait()
+			cancel()
+			close(cookieChan)
+		}()
+		for cookieStore, err := range s {
+			if err != nil {
 				select {
 				case <-ctx.Done():
 					return
-				default:
+				case cookieChan <- ce{e: fmt.Errorf(`cookie store: %w`, err)}:
 				}
-				wgTrav.Add(1)
-				go func(cookieStore CookieStore) {
-					defer wgTrav.Done()
-					for cookie, err := range cookieStore.TraverseCookies(filters...) {
-						select {
-						case <-ctx.Done():
-							return
-						default:
-						}
-						cookieChan <- ce{c: cookie, e: err}
-					}
-				}(cookieStore)
+				continue
 			}
-		}()
-
-		wgTot.Add(runtime.NumCPU())
-		for range runtime.NumCPU() {
-			go func() {
-				defer wgTot.Done()
-				for {
+			if cookieStore == nil {
+				continue
+			}
+			wg.Add(1)
+			go func(cookieStore CookieStore) {
+				defer wg.Done()
+				for cookie, err := range cookieStore.TraverseCookies(filters...) {
 					select {
 					case <-ctx.Done():
 						return
-					case c, ok := <-cookieChan:
-						if !ok {
-							return
-						}
-						if !yield(c.c, c.e) {
-							cancel()
-							return
-						}
+					case cookieChan <- ce{c: cookie, e: err}:
 					}
 				}
-			}()
+			}(cookieStore)
+		}
+	}()
+
+	return func(yield func(*Cookie, error) bool) {
+		startChan <- struct{}{}
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case c, ok := <-cookieChan:
+				if !ok {
+					cancel()
+					return
+				}
+				if !yield(c.c, c.e) {
+					cancel()
+					return
+				}
+			}
 		}
 	}
 }
 
 func TraverseCookieStores(ctx context.Context) CookieStoreSeq {
-	return func(yield func(CookieStore, error) bool) {
+	ctx, cancel := context.WithCancel(ctx)
+	type se struct {
+		s CookieStore
+		e error
+	}
+	startChan := make(chan struct{}, 1)
+	storeChan := make(chan se, 1)
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			return
+		case <-startChan:
+		}
+
 		var wg sync.WaitGroup
 		wg.Add(len(finders))
-
-		c := make(chan CookieStore)
-		done := make(chan struct{})
-
-		go func() {
-			for cookieStore := range c {
-				select {
-				case <-ctx.Done():
-					return
-				default:
-				}
-				if !yield(cookieStore, nil) {
-					return
-				}
-
-			}
-			close(done)
+		defer func() {
+			wg.Wait()
+			cancel()
+			close(storeChan)
 		}()
 
 		muFinder.RLock()
 		defer muFinder.RUnlock()
+
 		for _, finder := range finders {
-			select {
-			case <-ctx.Done():
-				return
-			default:
+			if finder == nil {
+				wg.Done()
+				continue
 			}
 			go func(finder CookieStoreFinder) {
 				defer wg.Done()
 				for cookieStore, err := range finder.FindCookieStores() {
-					if err == nil && cookieStore != nil {
-						c <- cookieStore
+					select {
+					case <-ctx.Done():
+						return
+					case storeChan <- se{s: cookieStore, e: err}:
 					}
 				}
 			}(finder)
 		}
+	}()
 
-		wg.Wait()
-		close(c)
-
-		<-done
+	return func(yield func(CookieStore, error) bool) {
+		startChan <- struct{}{}
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case s, ok := <-storeChan:
+				if !ok {
+					cancel()
+					return
+				}
+				if !yield(s.s, s.e) {
+					cancel()
+					return
+				}
+			}
+		}
 	}
 }

browser/opera/cookies4dat.go
@@ -67,11 +67,13 @@ func (s *operaPrestoCookieStore) TraverseCookies(filters ...kooky.Filter) kooky.
 			return iterx.CookieFilterYield(context.Background(), cookie, err, yield, filters...)
 		}
 		_, err := p.process(yld)
-		if err != nil && err != io.EOF {
-			_ = yld(nil, err)
+		if err != nil {
+			if !p.end && !errors.Is(err, iterx.ErrYieldEnd) && !errors.Is(err, io.EOF) {
+				_ = yld(nil, err)
+			}
 			return
 		}
-		if !yld(p.cookie, nil) {
+		if p.end || !yld(p.cookie, nil) {
 			return
 		}
 	}
@@ -88,12 +90,15 @@ type processor struct {
 	cookie        *kooky.Cookie
 	filters       []kooky.Filter
 	browser       kooky.BrowserInfo
+	end           bool
 }
 
 func (p *processor) process(yield func(*kooky.Cookie, error) bool) (int, error) {
 	if p.idTagLength < 1 || p.idTagLength > 4 || p.lengthLength < 1 || p.lengthLength > 4 {
 		err := errors.New(`unexpected byte length values`)
-		yield(nil, err)
+		if !yield(nil, err) {
+			p.end = true
+		}
 		return 0, err
 	}
 
@@ -137,6 +142,7 @@ func (p *processor) process(yield func(*kooky.Cookie, error) bool) (int, error) 
 		c.Path = p.path
 		c.Browser = p.browser
 		if !yield(p.cookie, nil) {
+			p.end = true
 			return n, err
 		}
 		p.cookie = c

filter.go
@@ -324,7 +324,7 @@ var HTTPOnly Filter = FilterFunc(func(cookie *Cookie) bool {
 
 // expires filters
 
-var Valid Filter = FilterFunc(func(cookie *Cookie) bool {
+var Valid Filter = ValueFilterFunc(func(cookie *Cookie) bool {
 	return cookie != nil && cookie.Expires.After(time.Now()) && cookie.Cookie.Valid() == nil
 })
 

firstmatch_nid_test.go
@@ -1,0 +1,27 @@
+package kooky
+
+import (
+	"context"
+	"testing"
+)
+
+var nidFilters = []Filter{
+	Domain(`.google.com`),
+	Name(`NID`),
+}
+
+func BenchmarkFirstMatch(b *testing.B) {
+	ctx := context.Background()
+	for i := 0; i < b.N; i++ {
+		cookie := TraverseCookies(ctx).FirstMatch(ctx, nidFilters...)
+		_ = cookie
+	}
+}
+
+func BenchmarkFirstMatchSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		// cookies := ReadCookies(nidFilters...) // old name
+		cookies := AllCookies(nidFilters...)
+		_ = cookies
+	}
+}

go.sum
@@ -48,26 +48,17 @@ github.com/stretchr/testify v1.7.1/go.mod h1:6Fq8oRcR53rry900zMqJjRRixrwX3KX962/
 github.com/stretchr/testify v1.8.0/go.mod h1:yNjHg4UonilssWZ8iaSj1OCr/vHnekPRkoO+kdMU+MU=
 github.com/stretchr/testify v1.8.1/go.mod h1:w2LPCIKwWwSfY2zedu0+kehJoqGctiVI29o6fzry7u4=
 github.com/stretchr/testify v1.8.4 h1:CcVxjf3Q8PM0mHUKJCdn+eZZtm5yQwehR5yeSVQQcUk=
-github.com/zalando/go-keyring v0.2.4 h1:wi2xxTqdiwMKbM6TWwi+uJCG/Tum2UV0jqaQhCa9/68=
-github.com/zalando/go-keyring v0.2.4/go.mod h1:HL4k+OXQfJUWaMnqyuSOc0drfGPX2b51Du6K+MRgZMk=
+github.com/stretchr/testify v1.8.4/go.mod h1:sz/lmYIOXD/1dqDmKjjqLyZ2RngseejIcXlSw2iwfAo=
 github.com/zalando/go-keyring v0.2.5 h1:Bc2HHpjALryKD62ppdEzaFG6VxL6Bc+5v0LYpN8Lba8=
 github.com/zalando/go-keyring v0.2.5/go.mod h1:HL4k+OXQfJUWaMnqyuSOc0drfGPX2b51Du6K+MRgZMk=
-golang.org/x/crypto v0.23.0 h1:dIJU/v2J8Mdglj/8rJ6UUOM3Zc9zLZxVZwwxMooUSAI=
-golang.org/x/crypto v0.23.0/go.mod h1:CKFgDieR+mRhux2Lsu27y0fO304Db0wZe70UKqHu0v8=
 golang.org/x/crypto v0.24.0 h1:mnl8DM0o513X8fdIkmyFE/5hTYxbwYOjDS/+rK6qpRI=
 golang.org/x/crypto v0.24.0/go.mod h1:Z1PMYSOR5nyMcyAVAIQSKCDwalqy85Aqn1x3Ws4L5DM=
-golang.org/x/net v0.25.0 h1:d/OCCoBEUq33pjydKrGQhw7IlUPI2Oylr+8qLx49kac=
-golang.org/x/net v0.25.0/go.mod h1:JkAGAh7GEvH74S6FOH42FLoXpXbE/aqXSrIQjXgsiwM=
 golang.org/x/net v0.26.0 h1:soB7SVo0PWrY4vPW/+ay0jKDNScG2X9wFeYlXIvJsOQ=
 golang.org/x/net v0.26.0/go.mod h1:5YKkiSynbBIh3p6iOc/vibscux0x38BZDkn8sCUPxHE=
 golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.1.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
-golang.org/x/sys v0.20.0 h1:Od9JTbYCk261bKm4M/mw7AklTlFYIa0bIp9BgSm1S8Y=
-golang.org/x/sys v0.20.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
 golang.org/x/sys v0.21.0 h1:rF+pYz3DAGSQAxAu1CbC7catZg4ebC4UIeIhKxBZvws=
 golang.org/x/sys v0.21.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
-golang.org/x/text v0.15.0 h1:h1V/4gjBv8v9cjcR6+AR5+/cIYK5N/WAgiv4xlsEtAk=
-golang.org/x/text v0.15.0/go.mod h1:18ZOQIKpY8NJVqYksKHtTdi31H5itFRjB5/qKTNYzSU=
 golang.org/x/text v0.16.0 h1:a94ExnEXNtEwYLGJSIUxnWoxoRz/ZcCsV63ROupILh4=
 golang.org/x/text v0.16.0/go.mod h1:GhwF1Be+LQoKShO3cGOHzqOgRrGaYc9AvblQOmPVHnI=
 gopkg.in/alecthomas/kingpin.v2 v2.2.6/go.mod h1:FMv+mEhP44yOT+4EoQTLFTRgOQ1FBLkstjWtayDeSgw=

internal/iterx/iter.go
@@ -11,6 +11,9 @@ import (
 func CookieFilterYield(ctx context.Context, cookie *kooky.Cookie, errCookie error, yield func(*kooky.Cookie, error) bool, filters ...kooky.Filter) bool {
 	ret := true
 	if errCookie != nil {
+		if errors.Is(errCookie, ErrYieldEnd) {
+			return false
+		}
 		ret = yield(nil, errCookie)
 	}
 	if kooky.FilterCookie(ctx, cookie, filters...) {
@@ -71,4 +74,5 @@ func ErrCookieSeq(e error) kooky.CookieSeq {
 	return func(yield func(*kooky.Cookie, error) bool) { yield(nil, e) }
 }
 
+// this error should not surface to the library user
 var ErrYieldEnd = errors.New(`yield end`)

kooky.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"iter"
 	"net/http"
-	"runtime"
 	"sync"
 	"time"
 )
@@ -43,69 +42,7 @@ func AllCookies(filters ...Filter) Cookies {
 type CookieSeq iter.Seq2[*Cookie, error]
 
 func TraverseCookies(ctx context.Context, filters ...Filter) CookieSeq {
-	return func(yield func(*Cookie, error) bool) {
-		ctx, cancel := context.WithCancel(ctx)
-		type ce struct {
-			c *Cookie
-			e error
-		}
-		cookieChan := make(chan ce, 1)
-
-		var wgTot sync.WaitGroup
-		defer wgTot.Wait()
-		wgTot.Add(1)
-		go func() {
-			defer wgTot.Done()
-
-			var wgTrav sync.WaitGroup
-			defer func() {
-				wgTrav.Wait()
-				cancel()
-				close(cookieChan)
-			}()
-			for cookieStore, _ := range TraverseCookieStores(ctx) {
-				select {
-				case <-ctx.Done():
-					return
-				default:
-				}
-				wgTrav.Add(1)
-				go func(cookieStore CookieStore) {
-					defer wgTrav.Done()
-					for cookie, err := range cookieStore.TraverseCookies(filters...) {
-						select {
-						case <-ctx.Done():
-							return
-						default:
-						}
-						cookieChan <- ce{c: cookie, e: err}
-					}
-				}(cookieStore)
-			}
-		}()
-
-		wgTot.Add(runtime.NumCPU())
-		for range runtime.NumCPU() {
-			go func(yield func(*Cookie, error) bool) {
-				defer wgTot.Done()
-				for {
-					select {
-					case <-ctx.Done():
-						return
-					case c, ok := <-cookieChan:
-						if !ok {
-							cancel()
-							return
-						}
-						if !yield(c.c, c.e) {
-							cancel()
-							return
-						}
-					}
-				}
-			}(yield)
-		}
-	}
+	return TraverseCookieStores(ctx).TraverseCookies(ctx, filters...)
 }
 
 // Collect() is the same as ReadAllCookies but ignores the error

commit befc6c92c9ad8de240553b388613a47d250b346a

fix: remove cancel() from producer goroutine defers in find.go
The goroutine defers in TraverseCookieStores and
CookieStoreSeq.TraverseCookies called cancel() before close(ch).
When items remained in the channel buffer, the consumer's select
could pick ctx.Done() over the channel read, losing data.

Race introduced in bf04c09. Fix: let close(ch) signal the consumer;
only the consumer calls cancel().

find.go
@@ -124,7 +124,6 @@ func (s CookieStoreSeq) TraverseCookies(ctx context.Context, filters ...Filter) 
 		var wg sync.WaitGroup
 		defer func() {
 			wg.Wait()
-			cancel()
 			close(cookieChan)
 		}()
 		for cookieStore, err := range s {
@@ -195,7 +194,6 @@ func TraverseCookieStores(ctx context.Context) CookieStoreSeq {
 		var wg sync.WaitGroup
 		defer func() {
 			wg.Wait()
-			cancel()
 			close(storeChan)
 		}()
 